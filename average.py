15d584c90076c5ded3e3ddc655f4dcd66da5ac6e
Sharad Jadhav <130732706+sharadjadhav02@users.noreply.github.com>
2023-05-22 11:47:12 +0530

    Delete average.py

--- average.py
+++
@@ -1,4 +0,0 @@
-reg_number = input("Enter Registration Number: ")
-m1, m2, m3 = map(int, input("Enter Grades: ").split())
-avg = (m1 + m2 + m3)/3
-print("Registration Number: {} \nAverage Grades: {} ".format(reg_number, avg))
